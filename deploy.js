1ea06ee636394208e10222e7c25f05c985d0ff88
vallabhiaf <vallabh.iaf123@gmail.com>
2019-03-30 15:34:24 +0530

    Add files via upload

---
+++ deploy.js
@@ -0,0 +1,22 @@
+const HDWalletProvider = require('truffle-hdwallet-provider');
+const Web3 = require('web3');
+const { interface, bytecode } = require('./compile');
+
+const provider = new HDWalletProvider(
+  'sphere deny swallow vivid symbol robot pizza remember panel tube mesh cruise',
+  'https://rinkeby.infura.io/v3/3b8defe1efdb4d668b2bbb50a9d16807'
+);
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+  const accounts = await web3.eth.getAccounts();
+
+  console.log('Attempting to deploy from account', accounts[0]);
+
+  const result = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({ data: '0x' + bytecode })
+    .send({ gas: '1000000', from: accounts[0] });
+
+  console.log('Contract deployed to', result.options.address);
+};
+deploy();
